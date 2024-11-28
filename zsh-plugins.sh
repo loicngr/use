b29d6d74daf568c73c41269d08a77761822c8863
lnogier <loicnogier@ik.me>
2024-11-28 13:48:47 +0100

    Update zsh-plugins.sh

--- zsh-plugins.sh
+++ zsh-plugins.sh
@@ -4,3 +4,6 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/you-should-use
 git clone https://github.com/fdellwing/zsh-bat.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-bat
+
+# https://docs.atuin.sh/guide/installation/
+curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
